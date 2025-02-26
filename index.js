941d373505129ec81aacda932a5ffe72b0cf8cda
AndreSS <108243074+AndreSS1022@users.noreply.github.com>
2025-02-25 19:04:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-// en javascript hay dos formas de declarar funciones
-// 1. Clasica
-function searchSong () 
-  {
-    
-  }
+/* en javascript hay dos formas
+ de declarar funciones
 
-// 2. Moderna
-const searchSong = () =>
-  {
-    //se guarda el valor digitado por el usuario en el html y se guarda en la variable SongName
-    let songName = document.getElementById('search-field').value
-    
-    //Se envia la variable songName al api para consultar la informacion de la cancion o artista
-    fetch(`https://api.lyrics.ovh/suggest/${songName}`.then(responce => responce.json())
-    .then(data => displaySong(data.data))
+ 1 //
+function searchSong()
+{
 
-          const displaySong = songs =>
-            {
-              const songContainer = document.
-              getElementById('song-container') 
-              songContainer.innerHTML = ''
-            }
+}*/
+
+//2. moderna
+const searchSong =() =>
     
-  }
+{
+     // se guardar el valor digitado por
+     // el usuario en el html y se guardar
+     // en la variable songName
+
+    let songName = document.getElementById
+    ('search-field').value
+
+
+    //se envia la variable songName al api
+    // para consultar la informacion de la 
+    // cancion o artista
+
+    
+    fetch(`https://api.lyrics.ovh/suggest/
+        ${songName}`)
+
+        .then(res => res.json())
+        .then(data => displaySong(data.data))
+
+}
+
+
+const displaySong = songs => {
+    const songContainer = document.getElementById("song-container")
+
+
+}
